37e0b288b748289e78b8ae5a32eb6fad15abf4b0
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-06-19 19:31:10 +0530

    Logout Bug Fixed

--- public/js/login.js
+++ public/js/login.js
@@ -33,7 +33,7 @@ export const logout = async () => {
     if (res.data.status === 'success') {
       showAlert('success', 'Logged out successfully!');
       window.setTimeout(() => {
-        location.assign('/');
+        location.reload(true);
       }, 1500);
     }
     
