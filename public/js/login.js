4e2021419fe9b468bcbad9db7f5a7296ee3a9fad
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-06-19 22:26:03 +0530

    Logout bug fixed

--- public/js/login.js
+++ public/js/login.js
@@ -28,12 +28,11 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: '/api/v1/users/logout'
+      url: '/api/v1/users/logout',
     });
     if (res.data.status === 'success') {
-      location.reload()
+      location.reload();
     }
-    
   } catch (err) {
     console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
