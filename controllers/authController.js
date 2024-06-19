69dad5396f710eb3c13ca61dfa26e4ddf8f32056
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-06-19 22:31:48 +0530

    Logout bug fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,11 +73,16 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, req, res);
 });
 
+// exports.logout = (req, res) => {
+//   res.cookie('jwt', 'loggedout', {
+//     expires: new Date(Date.now() + 10 * 1000),
+//     httpOnly: true
+//   });
+//   res.status(200).json({ status: 'success' });
+// };
+
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
-  });
+  res.clearCookie('jwt');
   res.status(200).json({ status: 'success' });
 };
 
