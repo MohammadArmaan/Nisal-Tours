fc07f61df57f91912722ceace780c1fdc1446f85
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-06-20 20:27:13 +0530

    Bookings bug fixed

--- public/js/index.js
+++ public/js/index.js
@@ -178,7 +178,7 @@ if (bookBtn)
   });
 
 
-  const alertMessage = document.querySelector('body').dataset.alert;
-  if(alertMessage){
-    showAlert('success', alertMessage);
-  }
+  // const alertMessage = document.querySelector('body').dataset.alert;
+  // if(alertMessage){
+  //   showAlert('success', alertMessage);
+  // }
