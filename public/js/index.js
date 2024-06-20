4b5daefec7a4b0bac84cfac8686b54656f427b0a
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2024-06-20 19:51:33 +0530

    Bookings alert message

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,7 @@ import { forgotPassword } from './forgotPassword';
 import { resetPassword } from './resetPassword';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -175,3 +176,9 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+
+  const alertMessage = document.querySelector('body').dataset.alert;
+  if(alertMessage){
+    showAlert('success', alertMessage, 10);
+  }
